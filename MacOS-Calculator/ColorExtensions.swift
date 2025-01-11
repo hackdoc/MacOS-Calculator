2e667393fba2d9e2534dda76e179a551eb56f493
perez987 <eaperez@me.com>
2025-01-12 00:55:53 +0100

    Colors defined as division instead of decimal

--- MacOS-Calculator/ColorExtensions.swift
+++ MacOS-Calculator/ColorExtensions.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 extension Color {
-        static let Gray = Color(red: 0.56, green: 0.56, blue: 0.56)
-        static let Orange = Color(red: 0.96, green: 0.46, blue: 0.01)
-        static let DarkGray = Color(red: 0.18, green: 0.18, blue: 0.18)
+        static let Gray = Color(red: 100/255, green: 100/255, blue: 100/255) // macOS calculator rgb(105,108,115)
+        static let Orange = Color(red: 250/255, green: 112/255, blue: 16/255) // macOSX calculator rgb(240,145,16)
+        static let DarkGray = Color(red: 51/255, green: 51/255, blue: 51/255) // macOS calculator rgb(75,78,85)
     }
